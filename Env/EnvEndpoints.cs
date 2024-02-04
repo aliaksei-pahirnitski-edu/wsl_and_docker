71f619da9eccdd106da4cd7aced2a3ad0b1ca4f8
Aliaksei Pahirnitski <aliaksei.pahirnitski@soxes.ch>
2024-02-04 10:10:58 +0300

    Adding show-cases to check in linux/wsl/docker

---
+++ Env/EnvEndpoints.cs
@@ -0,0 +1,10 @@
+﻿
+namespace wsl_and_docker.Env
+{
+    public static class EnvEndpoints
+    {
+        public static string OSName() => Environment.OSVersion.ToString();
+
+        internal static string? GetEnv(string envName) => Environment.GetEnvironmentVariable(envName);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using wsl_and_docker.Env;
+using wsl_and_docker.Weather;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,31 +17,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
+app.MapGet("/weatherforecast", WeatherEndpoint.GetForecast)
 .WithName("GetWeatherForecast")
 .WithOpenApi();
 
+app.MapGet("/os", EnvEndpoints.OSName);
+app.MapGet("/env", EnvEndpoints.GetEnv);
+
+
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
