7b594f7076da0abc587300f7c0fa40fd3ee2abe6
aliaksei-pahirnitski-edu <aliaksei.pahirnitski.edu@gmail.com>
2024-02-12 07:55:39 +0300

    Add NBomber

---
+++ src/nbomber/ConsoleAppNBomber/Program.cs
@@ -0,0 +1,62 @@
+﻿using NBomber.CSharp;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+using var httpClient = new HttpClient();
+httpClient.BaseAddress = new Uri("http://localhost:5049/");
+
+var scenarioOs = Scenario.Create("get_os_name", async context =>
+{
+    var response = await httpClient.GetAsync("/os/");
+
+    return response.IsSuccessStatusCode
+        ? Response.Ok()
+        : Response.Fail();
+})
+        .WithoutWarmUp()
+        .WithLoadSimulations(
+            Simulation.RampingInject(rate: 5,
+                                interval: TimeSpan.FromSeconds(1),
+                                during: TimeSpan.FromSeconds(30))
+        );
+
+
+
+var scenarioCpu = Scenario.Create("eating CPU", async context =>
+{
+    var response = await httpClient.GetAsync("/cpu");
+
+    return response.IsSuccessStatusCode
+        ? Response.Ok()
+        : Response.Fail();
+})
+            .WithoutWarmUp()
+            .WithLoadSimulations(
+                Simulation.Inject(rate: 2,
+                                  interval: TimeSpan.FromSeconds(1),
+                                  during: TimeSpan.FromSeconds(30))
+            );
+/*
+var scenarioWeather = Scenario.Create("get_weatherforecast", async context =>
+{
+    var response = await httpClient.GetAsync("/weatherforecast/");
+
+    return response.IsSuccessStatusCode
+        ? Response.Ok()
+        : Response.Fail();
+})
+            .WithoutWarmUp()
+            .WithLoadSimulations(
+                Simulation.Inject(rate: 2,
+                                  interval: TimeSpan.FromSeconds(1),
+                                  during: TimeSpan.FromSeconds(30))
+            );
+*/
+
+NBomberRunner
+    .RegisterScenarios(scenarioOs, scenarioCpu)
+    .Run();
+
+Console.WriteLine("Press any key ...");
+Console.ReadKey();
